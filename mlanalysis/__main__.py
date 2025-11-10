6d499191b2c5b1591ecc4ada070ae6e425379bcc
bobby-payne <gpayne1654@uvic.ca>
2025-11-10 09:50:43 -0800

    feat: produce figure of multiple downscaled realizations for a given time

--- mlanalysis/__main__.py
+++ mlanalysis/__main__.py
@@ -1,8 +1,10 @@
-import numpy as np
+import time
 
 from .experiment import Experiment
-from .figures import plot_metrics
+from .figures import plot_metrics, plot_realizations
 
+
+start_time = time.time()
 
 # plot the training and validation metrics
 experiment = Experiment()
@@ -10,3 +12,8 @@ experiment.summary()
 
 # plot training and validation metrics
 plot_metrics(experiment)
+plot_realizations(experiment, var='fwi', N=4, time_idx=None, seed=242)
+
+finish_time = time.time()
+elapsed_time = finish_time - start_time
+print(f"Finished in {elapsed_time/60:.2f} minutes.")
--- mlanalysis/figures.py
+++ mlanalysis/figures.py
@@ -3,6 +3,8 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
+from .utils import set_seed
+
 
 def _save_figure(fig, filename, experiment):
 
@@ -45,3 +47,57 @@ def plot_metrics(experiment):
         _save_figure(plt.gcf(), f"training_{m}", experiment)
 
     plt.close('all')
+
+
+def plot_realizations(experiment, var, N, time_idx, seed=None):
+
+    # set seed for reproducibility and choose time index if necessary
+    set_seed(seed)
+    if time_idx is None:
+        time_idx = np.random.randint(0, len(experiment.timestamps))
+
+    # Generate realizations
+    realizations = experiment.generate_realizations(time_idx=time_idx,
+                                                    N_realizations=N,
+                                                    seed=seed,
+                                                    unscale=True,
+                                                    round_negatives=False)
+    groundtruth_data = experiment.data_scaled['groundtruth']
+    datetime_str = experiment.timestamps[time_idx]
+
+    # Figure constants
+    fig, ax = plt.subplots(nrows=1, ncols=N+2, figsize=(8, 4), dpi=200)
+    ts = 8
+    vmin = 0
+    vmax = 22
+    cmap = 'viridis'
+
+    # Plot the individual realizations
+    for i in range(N):
+        realization = realizations[i]
+        ax[i].imshow(realization, cmap=cmap, origin='lower', vmin=vmin, vmax=vmax)
+        ax[i].set_title(f"Realization {i+1}", fontsize=ts)
+        ax[i].axis('off')
+
+    # Plot the realization mean
+    realization_mean = (1/N)*sum(realizations)
+    ax[N].imshow(realization_mean, cmap=cmap, origin='lower', vmin=vmin, vmax=vmax)
+    ax[N].set_title("Realization Mean", fontsize=ts)
+    ax[N].axis('off')
+
+    # Plot the ground truth
+    ground_truth = groundtruth_data[var][time_idx].squeeze()
+    refplot = ax[N+1].imshow(ground_truth, cmap=cmap, origin='lower', vmin=vmin, vmax=vmax)
+    ax[N+1].set_title("Ground Truth", fontsize=ts)
+    ax[N+1].axis('off')
+
+    # Add datetime annotation... (11,3) for bottom right, (11,30) for top right
+    ax[0].text(0, -5, f'{datetime_str[:10]} at {datetime_str[11:]}00', fontsize=.8*ts, color='red', verticalalignment='top', horizontalalignment='left')
+
+    # Add colorbar (using the last plotted image for the colorbar)
+    colorbar = fig.colorbar(refplot, ax=ax, orientation='horizontal', pad=0.05, aspect=140)
+    colorbar.outline.set_visible(False)
+    colorbar.ax.tick_params(labelsize=5, width=.25)
+    colorbar.ax.set_title(f"{var.upper()}",fontsize=ts, pad=2.5)
+
+    _save_figure(fig, f"realizations_{var}_{datetime_str}", experiment)
