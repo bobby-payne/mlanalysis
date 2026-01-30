9db9276b8bfae166561428a26a85ffb8697cbfea
bobby-payne <gpayne1654@uvic.ca>
2026-01-30 13:54:26 -0800

    refactor cli.py, selecting gpu now works as intended

--- mlanalysis/cli.py
+++ mlanalysis/cli.py
@@ -1,61 +1,71 @@
+import os
 import time
 import gc
-import subprocess
 
-from .experiment import Experiment
-from .utils import set_seed
-from .figures import (
-    plot_metrics,
-    plot_realizations,
-    plot_realizations_spectra,
-    plot_timeseries,
-    plot_dailymax_timeseries,
-    plot_rank_histogram,
-    plot_pixelwise_statistics,
-    plot_pixelwise_statistics_histogram,
-    plot_time_avg_spectrum,
-    plot_spectrogram,
-)
+from .config import get_config
 
 
 def main():
 
     start_time = time.time()
 
-    # plot the training and validation metrics
-    experiment = Experiment()
-    experiment.summary()
+    try:
 
-    # Set random seed for reproducibility & select visible GPU
-    set_seed(experiment.seed)
-    if experiment.which_gpu is not None:
-        subprocess.run(["export", f"CUDA_VISIBLE_DEVICES={experiment.which_gpu}"], shell=True)
+        # Select visible GPUs (must occur before calling Experiment class)
+        which_gpu = get_config()['which_gpu']
+        if which_gpu is not None and "CUDA_VISIBLE_DEVICES" not in os.environ:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(which_gpu)
 
-    # Produce plots
-    N = experiment.n_realizations
-    plot_metrics(experiment)
+        # Import modules with Torch dependency
+        # (must be AFTER setting CUDA_VISIBLE_DEVICES)
+        from .experiment import Experiment
+        from .utils import set_seed
+        from .figures import (
+            plot_metrics,
+            plot_realizations,
+            plot_realizations_spectra,
+            plot_timeseries,
+            plot_dailymax_timeseries,
+            plot_rank_histogram,
+            plot_pixelwise_statistics,
+            plot_pixelwise_statistics_histogram,
+            plot_time_avg_spectrum,
+            plot_spectrogram,
+        )
 
-    for var in experiment.predictand_names:
+        # Set random seed for reproducibility
+        set_seed(get_config()['seed'])
 
-        plot_realizations(experiment, var=var, N=4, time_idx=-1)
-        plot_realizations_spectra(experiment, var=var, N=N, time_idx=-1)
-        plot_pixelwise_statistics(experiment, var=var, N=N, daily_max=False)
-        plot_pixelwise_statistics(experiment, var=var, N=N, daily_max=True)
-        plot_pixelwise_statistics_histogram(experiment, var=var, N=N, daily_max=False)
-        plot_pixelwise_statistics_histogram(experiment, var=var, N=N, daily_max=True)
-        plot_time_avg_spectrum(experiment, var=var, N=N, daily_max=False)
-        plot_time_avg_spectrum(experiment, var=var, N=N, daily_max=True)
-        plot_spectrogram(experiment, var=var, N=N, daily_max=False)
-        plot_spectrogram(experiment, var=var, N=N, daily_max=True)
+        # plot the training and validation metrics
+        experiment = Experiment()
+        experiment.summary()
 
-        for loc in experiment.loc_idxs:  # Location-specific plots
+        # Produce plots
+        N = experiment.n_realizations
+        plot_metrics(experiment)
 
-            plot_timeseries(experiment, var=var, N=N, xy=loc)
-            plot_dailymax_timeseries(experiment, var=var, N=N, xy=loc)
-            plot_rank_histogram(experiment, var=var, N=N, xy=loc, daily_max=False)
-            plot_rank_histogram(experiment, var=var, N=N, xy=loc, daily_max=True)
+        for var in experiment.predictand_names:
 
-    gc.collect()
-    finish_time = time.time()
-    elapsed_time = finish_time - start_time
-    print(f"Finished in {elapsed_time/60:.2f} minutes.")
+            plot_realizations(experiment, var=var, N=4, time_idx=-1)
+            plot_realizations_spectra(experiment, var=var, N=N, time_idx=-1)
+            plot_pixelwise_statistics(experiment, var=var, N=N, daily_max=False)
+            plot_pixelwise_statistics(experiment, var=var, N=N, daily_max=True)
+            plot_pixelwise_statistics_histogram(experiment, var=var, N=N, daily_max=False)
+            plot_pixelwise_statistics_histogram(experiment, var=var, N=N, daily_max=True)
+            plot_time_avg_spectrum(experiment, var=var, N=N, daily_max=False)
+            plot_time_avg_spectrum(experiment, var=var, N=N, daily_max=True)
+            plot_spectrogram(experiment, var=var, N=N, daily_max=False)
+            plot_spectrogram(experiment, var=var, N=N, daily_max=True)
+
+            for loc in experiment.loc_idxs:  # Location-specific plots
+
+                plot_timeseries(experiment, var=var, N=N, xy=loc)
+                plot_dailymax_timeseries(experiment, var=var, N=N, xy=loc)
+                plot_rank_histogram(experiment, var=var, N=N, xy=loc, daily_max=False)
+                plot_rank_histogram(experiment, var=var, N=N, xy=loc, daily_max=True)
+
+    finally:
+
+        gc.collect()
+        elapsed_time = time.time() - start_time
+        print(f"Finished in {elapsed_time/60:.2f} minutes.")
